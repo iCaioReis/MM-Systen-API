77d9453737ac6b80e0f7ec6496f8eb10a1aeefdb
iCaioReis <caioreis3186@gmail.com>
2024-07-25 09:23:47 -0300

    create createProofs migration

---
+++ src/database/knex/migrations/20240725115927_createProofs.js
@@ -0,0 +1,19 @@
+exports.up = knex => knex.schema.createTable("proofs", table => {
+    table.increments("id");
+
+    table
+        .enum("state", ["active", "making_registrations", "finished_inscriptions", "inative"], { useNative: true, enumName: "states" })
+        .notNullable()
+        .defaultTo("active");
+
+        table
+        .enum("name", ["seis_balizas", "tres_tambores", "maneabilidade"], { useNative: true, enumName: "names" })
+        .notNullable();
+
+        table.integer("event_id").references("id").inTable("events");
+   
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+});
+
+exports.down = knex => knex.schema.dropTable("proofs");
